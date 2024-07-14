113a70a979663ccf1b27738dcf26ec623f12959f
Biniyam Ajaw <91191700+biniyam69@users.noreply.github.com>
2024-07-14 06:53:19 +0100

    Update embed.js

--- embed.js
+++ embed.js
@@ -1,6 +1,6 @@
 (function() {
     const script = document.createElement('script');
-    script.src = 'https://biniyam69.github.io/embed-files/index-BEvdg_U9.js';
+    script.src = 'https://biniyam69.github.io/embed-files/index-DFz3uNR9.js';
     script.onload = () => {
       window.renderChatWidgetBizai('chat-widget-container');
     };
