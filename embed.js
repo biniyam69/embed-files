51d160ae3afb56f0954555801eef6b576b3120c6
Biniyam Ajaw <91191700+biniyam69@users.noreply.github.com>
2024-08-01 07:51:51 +0100

    Update embed.js
    Monte embed

--- embed.js
+++ embed.js
@@ -1,6 +1,6 @@
 (function() {
     const script = document.createElement('script');
-    script.src = 'https://biniyam69.github.io/embed-files/index-Dr_0h91L.js';
+    script.src = 'https://biniyam69.github.io/embed-files/monte.js';
     script.onload = () => {
       window.renderChatWidgetBizai('chat-widget-container');
     };
